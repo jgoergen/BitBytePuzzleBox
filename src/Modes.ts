d681522b4cae280388ba3a2945aea28e554adc9b
Jeff Goergen <jeffg@wambatech.com>
2018-11-13 21:41:23 -0500

    updates

--- src/Modes.ts
+++ src/Modes.ts
@@ -1,5 +1,6 @@
 import Display from "./Display";
 import GPIO from "./GPIO";
+import Midi from "./Midi";
 
 export default class Modes {
 
@@ -9,6 +10,7 @@ export default class Modes {
     phraseIndex: number;
     lastChar: string;
     mode: number;
+    midi: Midi;
 
     public async init(gpio: GPIO, display: Display): Promise<void> {
 
@@ -16,6 +18,8 @@ export default class Modes {
 
         this.gpio = gpio;
         this.display = display;
+        this.midi = new Midi();
+        await this.midi.init(this.receiveMidiData);
         await this.setMode(0);
     }
 
@@ -31,8 +35,8 @@ export default class Modes {
 
     public async setMode(which: number): Promise<void> {
 
-        if (which > 1)
-            which = 1;
+        if (which > 2)
+            which = 2;
         
         if (which < 0)
             which = 0;
@@ -61,6 +65,10 @@ export default class Modes {
             case 1: 
                 await this.mode2Loop(touchStates);
                 break;
+
+            case 2: 
+                await this.mode3Loop(touchStates);
+                break;
         }
     }
 
@@ -73,23 +81,44 @@ export default class Modes {
 
             case 0: 
                 this.phrase = new Array<string>();
-                for(var i = 0; i < 72; i++)
-                    this.phrase.push("");
-                this.lastChar = "";
-                this.phraseIndex = 0;
-                break;
-
-            case 1: 
-                this.phrase = new Array<string>();
                 for(var i = 0; i < 8; i++)
                     this.phrase.push("");
                 this.lastChar = "";
                 this.phraseIndex = 0;
+                await this.display.clear();
+                break;
+
+            case 1: 
+                this.phrase = new Array<string>();
+                for(var i = 0; i < 72; i++)
+                    this.phrase.push("");
+                this.lastChar = "";
+                this.phraseIndex = 0;
+                await this.display.clear();
+                await this.display.setCursor(0, 0);
+                await this.display.sendString("Garbage!");
+                break;
+
+            case 2:
+
+                this.display.clear();
+
+                try{
+                    await this.midi.closeInputPort();
+                    this.conectedToMidi = await this.midi.openInputPort(0);
+                    await this.display.setCursor(0, 0);
+                    await this.display.sendString(`Midi! ${this.conectedToMidi ? "=)" : "=("}`);
+
+                } catch(e) {
+                    
+                    await this.display.setCursor(0, 0);
+                    await this.display.sendString(`Midi! O_o`);
+                }
                 break;
         }
     }
 
-    public async mode1Loop(touchStates: Array<boolean>): Promise<void> {
+    public async mode2Loop(touchStates: Array<boolean>): Promise<void> {
 
         let touchBinary = 
             touchStates
@@ -124,12 +153,12 @@ export default class Modes {
                 this.phraseIndex = 0;
         }
 
-        this.display.setCursor(0, 0);
-        this.display.sendString(
+        await this.display.setCursor(0, 0);
+        await this.display.sendString(
             this.phrase.join(""));
     }
 
-    public async mode2Loop(touchStates: Array<boolean>): Promise<void> {
+    public async mode1Loop(touchStates: Array<boolean>): Promise<void> {
         
         let touchBinary = 
             touchStates
@@ -164,12 +193,40 @@ export default class Modes {
                 this.phraseIndex = 0;
         }
 
-        this.display.setCursor(0, 0);
-        this.display.sendString("Puzzle Box!");
-        this.display.setCursor(2, 2);
-        this.display.sendString(touchBinary);
-        this.display.setCursor(2, 4);
-        this.display.sendString(
+        await this.display.setCursor(0, 0);
+        await this.display.sendString("Puzzle Box!");
+        await this.display.setCursor(2, 2);
+        await this.display.sendString(touchBinary);
+        await this.display.setCursor(2, 4);
+        await this.display.sendString(
             this.phrase.join(""));
+    }
+
+    private lineIndex: number = 0;
+    private conectedToMidi: boolean = false;
+
+    public receiveMidiData(deltaTime: any, message: any) {
+
+        if (this.mode != 2)
+            return;
+
+        // The message is an array of numbers corresponding to the MIDI bytes:
+        //   [status, data1, data2]
+        // https://www.cs.cf.ac.uk/Dave/Multimedia/node158.html has some helpful
+        // information interpreting the messages.
+
+        console.log('m:' + message + ' d:' + deltaTime);
+
+        this.display.setCursor(this.lineIndex, 0);
+        this.display.sendString(`< ${deltaTime}: ${message[0]}: ${message[1]}-${message[2]}`);
+
+        this.lineIndex ++;
+        if (this.lineIndex > 5)
+            this.lineIndex = 0;
+    }
+
+    public async mode3Loop(touchStates: Array<boolean>): Promise<void> {
+        
+        
     }
 };
